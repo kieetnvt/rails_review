13682062060725dee3a6ce7cbaa1ed92ed7a2878
Kiet Nguyen <kietdev38@gmail.com>
2016-02-15 10:29:34 +0700

    has_one associations

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160215020949) do
+ActiveRecord::Schema.define(version: 20160215025732) do
+
+  create_table "accounts", force: :cascade do |t|
+    t.integer  "supplier_id",    limit: 4
+    t.string   "account_number", limit: 255
+    t.datetime "created_at",                 null: false
+    t.datetime "updated_at",                 null: false
+  end
+
+  add_index "accounts", ["supplier_id"], name: "index_accounts_on_supplier_id", using: :btree
 
   create_table "customers", force: :cascade do |t|
     t.string   "name",       limit: 255
@@ -27,5 +36,11 @@ ActiveRecord::Schema.define(version: 20160215020949) do
   end
 
   add_index "orders", ["customer_id"], name: "index_orders_on_customer_id", using: :btree
+
+  create_table "suppliers", force: :cascade do |t|
+    t.string   "name",       limit: 255
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
+  end
 
 end
